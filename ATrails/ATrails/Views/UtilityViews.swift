050a3e75e75fb6defc37925f6fe54282aa5f255f
Johan <143431900+Jolvera2001@users.noreply.github.com>
2023-11-26 15:16:26 -0600

    Beginning Profile Tab

---
+++ ATrails/ATrails/Views/UtilityViews.swift
@@ -0,0 +1,25 @@
+//
+//  UtilityViews.swift
+//  ATrails
+//
+//  Created by Turing on 11/26/23.
+//
+
+import SwiftUI
+
+
+// These views are for things like
+// Create a Post <- this one will be interesting
+// Profile actions
+// Map actions
+struct UtilityViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UtilityViews_Previews: PreviewProvider {
+    static var previews: some View {
+        UtilityViews()
+    }
+}
