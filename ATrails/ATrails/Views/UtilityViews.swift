0a55616ab7d5c2a1b9a8830c1c96b832623ecd7d
Johan <143431900+Jolvera2001@users.noreply.github.com>
2023-12-04 18:28:11 -0600

    Able to get images
    Need to store in FireBase and Start to populate the Post collection in Firebase Firestore

--- ATrails/ATrails/Views/UtilityViews.swift
+++ ATrails/ATrails/Views/UtilityViews.swift
@@ -58,31 +58,42 @@ struct MakePost: View {
                         .font(.title2)
                         .padding(10)
                     }
-                    
-                    HStack(spacing: 20) {
-                        AsyncImage(url: URL(string: authController.currentUser?.profilePictureURL ?? ""))
-                            .frame(width: 80, height: 80)
-                            .clipShape(Circle())
+                    ScrollView {
+                        HStack(spacing: 20) {
+                            AsyncImage(url: URL(string: authController.currentUser?.profilePictureURL ?? ""))
+                                .frame(width: 80, height: 80)
+                                .clipShape(Circle())
+                            
+                            Text(authController.currentUser?.username ?? "Not Found")
+                                .foregroundColor(.white)
+                                .font(.title)
+                                .padding(5)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal, 25)
                         
-                        Text(authController.currentUser?.username ?? "Not Found")
-                            .foregroundColor(.white)
-                            .font(.title)
-                            .padding(5)
+                        if selectedImage != nil {
+                            Image(uiImage: selectedImage!)
+                                .resizable()
+                                .frame(width: 350, height: 200)
+                                .cornerRadius(5)
+                                .padding(.top, 10)
+                        } else {
+                            // nothing...
+                        }
+                        
+                        TextField("Share something on ATrails...", text: $textString, axis: .vertical)
+                            .padding()
+                            .font(.title3)
+                            .background(Color.white.opacity(0.15))
+                            .foregroundColor(.black)
+                            .cornerRadius(10)
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 10)
+                            .lineLimit(6, reservesSpace: true)
                     }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.horizontal, 25)
-                    
-                    TextField("Share something on ATrails...", text: $textString, axis: .vertical)
-                        .padding()
-                        .font(.title3)
-                        .background(Color.white.opacity(0.15))
-                        .foregroundColor(.black)
-                        .cornerRadius(10)
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 10)
-                        .lineLimit(6, reservesSpace: true)
+                    .frame(maxHeight: .infinity, alignment: .top)
                 }
-                .frame(maxHeight: .infinity, alignment: .top)
                 
                 VStack {
                     HStack(spacing: 30) {
@@ -98,7 +109,7 @@ struct MakePost: View {
                         .padding(10)
                         
                         Button {
-                            
+                            isImagePickerPresented.toggle()
                         } label: {
                             VStack {
                                 Label("", systemImage: "photo.fill.on.rectangle.fill")
