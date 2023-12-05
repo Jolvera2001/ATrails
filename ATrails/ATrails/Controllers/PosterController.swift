f7257d6f11c199159385afdaa4793ada64376b0f
Johan <143431900+Jolvera2001@users.noreply.github.com>
2023-12-04 18:46:07 -0600

    Added dependencies

--- ATrails/ATrails/Controllers/PosterController.swift
+++ ATrails/ATrails/Controllers/PosterController.swift
@@ -6,47 +6,12 @@
 //
 
 import Foundation
-import UIKit
-import SwiftUI
+import Firebase
 
 class PosterController: ObservableObject {
+    let db = Firestore.firestore()
+    
     
 }
 
-//struct ImagePicker: UIViewControllerRepresentable {
-//    @Binding var selectedImage: UIImage?
-//    @Environment(\.presentationMode) private var presentationMode
-//
-//    func makeCoordinator() -> Coordinator {
-//        Coordinator(self)
-//    }
-//
-//    func makeUIViewController(context: Context) -> UIImagePickerController {
-//        let picker = UIImagePickerController()
-//        picker.delegate = context.coordinator
-//        picker.sourceType = .photoLibrary
-//        return picker
-//    }
-//
-//    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
-//
-//    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-//        let parent: ImagePicker
-//
-//        init(_ parent: ImagePicker) {
-//            self.parent = parent
-//        }
-//
-//        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-//            if let pickedImage = info[.originalImage] as? UIImage {
-//                parent.selectedImage = pickedImage
-//            }
-//            parent.presentationMode.wrappedValue.dismiss()
-//        }
-//
-//        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-//            parent.presentationMode.wrappedValue.dismiss()
-//        }
-//    }
-//}
 
